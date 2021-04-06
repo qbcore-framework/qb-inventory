8de3835a69bfef17694c21e502c9aadd082dd5f2
GhzGarage <57848836+GhzGarage@users.noreply.github.com>
2021-04-05 19:44:27 -0500

    SQL File

---
+++ qb-inventory/qb-inventory.sql
@@ -0,0 +1,87 @@
+CREATE TABLE IF NOT EXISTS `playerammo` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `citizenid` varchar(255) NOT NULL,
+  `ammo` text DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `citizenid` (`citizenid`)
+) ENGINE=InnoDB AUTO_INCREMENT=3638 DEFAULT CHARSET=latin1;
+
+CREATE TABLE IF NOT EXISTS `gloveboxitems` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `plate` varchar(255) DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `amount` int(11) DEFAULT NULL,
+  `info` text DEFAULT NULL,
+  `type` varchar(255) DEFAULT NULL,
+  `slot` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
+
+CREATE TABLE IF NOT EXISTS `gloveboxitemsnew` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `plate` varchar(255) DEFAULT NULL,
+  `items` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `plate` (`plate`)
+) ENGINE=InnoDB AUTO_INCREMENT=1551 DEFAULT CHARSET=latin1;
+
+CREATE TABLE IF NOT EXISTS `stashitems` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `stash` varchar(255) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `amount` int(11) NOT NULL,
+  `info` text NOT NULL,
+  `type` varchar(255) NOT NULL,
+  `slot` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=15024 DEFAULT CHARSET=latin1;
+
+CREATE TABLE IF NOT EXISTS `stashitemsnew` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `stash` varchar(255) NOT NULL,
+  `items` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `stash` (`stash`)
+) ENGINE=InnoDB AUTO_INCREMENT=2109 DEFAULT CHARSET=latin1;
+
+CREATE TABLE IF NOT EXISTS `trunkitems` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `plate` varchar(255) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `amount` int(11) NOT NULL,
+  `info` text NOT NULL,
+  `type` varchar(255) NOT NULL,
+  `slot` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=633 DEFAULT CHARSET=latin1;
+
+CREATE TABLE IF NOT EXISTS `trunkitemsnew` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `plate` varchar(255) NOT NULL,
+  `items` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `plate` (`plate`)
+) ENGINE=InnoDB AUTO_INCREMENT=1127 DEFAULT CHARSET=latin1;
+
+CREATE TABLE IF NOT EXISTS `player_vehicles` (
+  `#` int(11) NOT NULL AUTO_INCREMENT,
+  `steam` varchar(50) DEFAULT NULL,
+  `citizenid` varchar(50) DEFAULT NULL,
+  `vehicle` varchar(50) DEFAULT NULL,
+  `hash` varchar(50) DEFAULT NULL,
+  `mods` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL,
+  `plate` varchar(50) NOT NULL,
+  `fakeplate` varchar(50) DEFAULT NULL,
+  `garage` varchar(50) DEFAULT NULL,
+  `fuel` int(11) DEFAULT 100,
+  `engine` float DEFAULT 1000,
+  `body` float DEFAULT 1000,
+  `state` int(11) DEFAULT 1,
+  `depotprice` int(11) NOT NULL DEFAULT 0,
+  `drivingdistance` int(50) DEFAULT NULL,
+  `status` text DEFAULT NULL,
+  PRIMARY KEY (`#`),
+  KEY `plate` (`plate`),
+  KEY `citizenid` (`citizenid`),
+  KEY `steam` (`steam`)
+) ENGINE=InnoDB AUTO_INCREMENT=2432 DEFAULT CHARSET=utf8mb4;
