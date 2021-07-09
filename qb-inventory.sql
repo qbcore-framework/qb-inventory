86cf8c159f9f84a29723b05344d00207913b6047
GhzGarage <gigahertzgarage@mail.com>
2021-07-08 19:52:55 -0500

    ⚠️⚠️ Steam Ditch ⚠️⚠️

--- qb-inventory.sql
+++ qb-inventory.sql
@@ -75,8 +75,8 @@ CREATE TABLE IF NOT EXISTS `trunkitemsnew` (
 ) ENGINE=InnoDB AUTO_INCREMENT=1127 DEFAULT CHARSET=latin1;
 
 CREATE TABLE IF NOT EXISTS `player_vehicles` (
-  `#` int(11) NOT NULL AUTO_INCREMENT,
-  `steam` varchar(50) DEFAULT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `license` varchar(50) DEFAULT NULL,
   `citizenid` varchar(50) DEFAULT NULL,
   `vehicle` varchar(50) DEFAULT NULL,
   `hash` varchar(50) DEFAULT NULL,
@@ -91,8 +91,8 @@ CREATE TABLE IF NOT EXISTS `player_vehicles` (
   `depotprice` int(11) NOT NULL DEFAULT 0,
   `drivingdistance` int(50) DEFAULT NULL,
   `status` text DEFAULT NULL,
-  PRIMARY KEY (`#`),
+  PRIMARY KEY (`id`),
   KEY `plate` (`plate`),
   KEY `citizenid` (`citizenid`),
-  KEY `steam` (`steam`)
+  KEY `license` (`license`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2432 DEFAULT CHARSET=utf8mb4;
