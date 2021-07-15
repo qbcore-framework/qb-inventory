1499101070827473aeb0d4eae0f196406d8ec02a
Karl <karljajakop@gmail.com>
2021-07-15 15:40:39 +0300

    New licenses + pictures

--- server/main.lua
+++ server/main.lua
@@ -1629,16 +1629,46 @@ QBCore.Functions.CreateUseableItem("id_card", function(source, item)
 end)
 
 QBCore.Functions.CreateUseableItem("weaponlicense", function(source, item)
-    for k, v in pairs(QBCore.Functions.GetPlayers()) do
-        local character = QBCore.Functions.GetPlayer(source)
-        local PlayerPed = GetPlayerPed(source)
-        local TargetPed = GetPlayerPed(v)
-        local dist = #(GetEntityCoords(PlayerPed) - GetEntityCoords(TargetPed))
-        if dist < 3.0 then
-            TriggerClientEvent('chat:addMessage', v,  {
-                template = '<div class="chat-message advert"><div class="chat-message-body"><strong>{0}:</strong><br><br> <strong>First Name:</strong> {1} <br><strong>Last Name:</strong> {2} <br><strong>Birth Date:</strong> {3} <br><strong>Licenses:</strong> {4}</div></div>',
-                args = {'Weapons License', character.PlayerData.charinfo.firstname, character.PlayerData.charinfo.lastname, character.PlayerData.charinfo.birthdate, item.info.type}
-            })
-        end
-    end
+	for k, v in pairs(QBCore.Functions.GetPlayers()) do
+		local character = QBCore.Functions.GetPlayer(source)
+		local PlayerPed = GetPlayerPed(source)
+		local TargetPed = GetPlayerPed(v)
+		local dist = #(GetEntityCoords(PlayerPed) - GetEntityCoords(TargetPed))
+		if dist < 3.0 then
+			TriggerClientEvent('chat:addMessage', v,  {
+				template = '<div class="chat-message advert"><div class="chat-message-body"><strong>{0}:</strong><br><br> <strong>First Name:</strong> {1} <br><strong>Last Name:</strong> {2} <br><strong>Birth Date:</strong> {3} <br><strong>Licenses:</strong> {4}</div></div>',
+				args = {'Weapons License', character.PlayerData.charinfo.firstname, character.PlayerData.charinfo.lastname, character.PlayerData.charinfo.birthdate, item.info.type}
+			})
+		end
+	end
+end)
+
+QBCore.Functions.CreateUseableItem("boat_license", function(source, item)
+	for k, v in pairs(QBCore.Functions.GetPlayers()) do
+		local character = QBCore.Functions.GetPlayer(source)
+		local PlayerPed = GetPlayerPed(source)
+		local TargetPed = GetPlayerPed(v)
+		local dist = #(GetEntityCoords(PlayerPed) - GetEntityCoords(TargetPed))
+		if dist < 3.0 then
+			TriggerClientEvent('chat:addMessage', v,  {
+				template = '<div class="chat-message advert"><div class="chat-message-body"><strong>{0}:</strong><br><br> <strong>First Name:</strong> {1} <br><strong>Last Name:</strong> {2} <br><strong>Birth Date:</strong> {3} <br><strong>Licenses:</strong> {4}</div></div>',
+				args = {'Watercraft License', character.PlayerData.charinfo.firstname, character.PlayerData.charinfo.lastname, character.PlayerData.charinfo.birthdate, item.info.type}
+			})
+		end
+	end
+end)
+
+QBCore.Functions.CreateUseableItem("plane_license", function(source, item)
+	for k, v in pairs(QBCore.Functions.GetPlayers()) do
+		local character = QBCore.Functions.GetPlayer(source)
+		local PlayerPed = GetPlayerPed(source)
+		local TargetPed = GetPlayerPed(v)
+		local dist = #(GetEntityCoords(PlayerPed) - GetEntityCoords(TargetPed))
+		if dist < 3.0 then
+			TriggerClientEvent('chat:addMessage', v,  {
+				template = '<div class="chat-message advert"><div class="chat-message-body"><strong>{0}:</strong><br><br> <strong>First Name:</strong> {1} <br><strong>Last Name:</strong> {2} <br><strong>Birth Date:</strong> {3} <br><strong>Licenses:</strong> {4}</div></div>',
+				args = {'Aircraft License', character.PlayerData.charinfo.firstname, character.PlayerData.charinfo.lastname, character.PlayerData.charinfo.birthdate, item.info.type}
+			})
+		end
+	end
 end)
