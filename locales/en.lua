e9584e03721beeb777e7392005f382a5860d802f
Martin Riggs <63644849+wanderrer@users.noreply.github.com>
2022-08-02 19:47:36 +0300

    Create en.lua
    Folder used to store language translations with english translation "template" (the file will be updated soon)

---
+++ locales/en.lua
@@ -0,0 +1,11 @@
+--[[
+Romanian base language translation for qb-inventory
+Translation done by wanderrer (Martin Riggs#0807 on Discord)
+]]--
+local Translations = {
+  
+  }
+Lang = Locale:new({
+    phrases = Translations,
+    warnOnMissing = true
+})
