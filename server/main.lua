27acc48fadf2dbc0df3ab20caaf8fd4046eb730f
BerkieBb <82737367+BerkieBb@users.noreply.github.com>
2022-08-17 12:52:58 +0200

    refactor(server); remove threads for item creation

--- server/main.lua
+++ server/main.lua
@@ -2207,65 +2207,62 @@ end, 'admin')
 
 --#region Items
 
--- We need this thread because otherwise qb-core will throw an error that the exports of qb-inventory are not loaded yet and trying to be used
-CreateThread(function()
-	CreateUsableItem("snowball", function(source, item)
-		local itemData = GetItemBySlot(source, item.slot)
-		if not itemData then return end
-		TriggerClientEvent("inventory:client:UseSnowball", source, itemData.amount)
-	end)
+CreateUsableItem("snowball", function(source, item)
+	local itemData = GetItemBySlot(source, item.slot)
+	if not itemData then return end
+	TriggerClientEvent("inventory:client:UseSnowball", source, itemData.amount)
+end)
 
-	CreateUsableItem("driver_license", function(source, item)
-		local playerPed = GetPlayerPed(source)
-		local playerCoords = GetEntityCoords(playerPed)
-		local players = QBCore.Functions.GetPlayers()
-		for _, v in pairs(players) do
-			local targetPed = GetPlayerPed(v)
-			local dist = #(playerCoords - GetEntityCoords(targetPed))
-			if dist < 3.0 then
-				TriggerClientEvent('chat:addMessage', v,  {
-						template = '<div class="chat-message advert"><div class="chat-message-body"><strong>{0}:</strong><br><br> <strong>First Name:</strong> {1} <br><strong>Last Name:</strong> {2} <br><strong>Birth Date:</strong> {3} <br><strong>Licenses:</strong> {4}</div></div>',
-						args = {
-							"Drivers License",
-							item.info.firstname,
-							item.info.lastname,
-							item.info.birthdate,
-							item.info.type
-						}
+CreateUsableItem("driver_license", function(source, item)
+	local playerPed = GetPlayerPed(source)
+	local playerCoords = GetEntityCoords(playerPed)
+	local players = QBCore.Functions.GetPlayers()
+	for _, v in pairs(players) do
+		local targetPed = GetPlayerPed(v)
+		local dist = #(playerCoords - GetEntityCoords(targetPed))
+		if dist < 3.0 then
+			TriggerClientEvent('chat:addMessage', v,  {
+					template = '<div class="chat-message advert"><div class="chat-message-body"><strong>{0}:</strong><br><br> <strong>First Name:</strong> {1} <br><strong>Last Name:</strong> {2} <br><strong>Birth Date:</strong> {3} <br><strong>Licenses:</strong> {4}</div></div>',
+					args = {
+						"Drivers License",
+						item.info.firstname,
+						item.info.lastname,
+						item.info.birthdate,
+						item.info.type
 					}
-				)
-			end
+				}
+			)
 		end
-	end)
+	end
+end)
 
-	CreateUsableItem("id_card", function(source, item)
-		local playerPed = GetPlayerPed(source)
-		local playerCoords = GetEntityCoords(playerPed)
-		local players = QBCore.Functions.GetPlayers()
-		for _, v in pairs(players) do
-			local targetPed = GetPlayerPed(v)
-			local dist = #(playerCoords - GetEntityCoords(targetPed))
-			if dist < 3.0 then
-				local gender = "Man"
-				if item.info.gender == 1 then
-					gender = "Woman"
-				end
-				TriggerClientEvent('chat:addMessage', v,  {
-						template = '<div class="chat-message advert"><div class="chat-message-body"><strong>{0}:</strong><br><br> <strong>Civ ID:</strong> {1} <br><strong>First Name:</strong> {2} <br><strong>Last Name:</strong> {3} <br><strong>Birthdate:</strong> {4} <br><strong>Gender:</strong> {5} <br><strong>Nationality:</strong> {6}</div></div>',
-						args = {
-							"ID Card",
-							item.info.citizenid,
-							item.info.firstname,
-							item.info.lastname,
-							item.info.birthdate,
-							gender,
-							item.info.nationality
-						}
-					}
-				)
+CreateUsableItem("id_card", function(source, item)
+	local playerPed = GetPlayerPed(source)
+	local playerCoords = GetEntityCoords(playerPed)
+	local players = QBCore.Functions.GetPlayers()
+	for _, v in pairs(players) do
+		local targetPed = GetPlayerPed(v)
+		local dist = #(playerCoords - GetEntityCoords(targetPed))
+		if dist < 3.0 then
+			local gender = "Man"
+			if item.info.gender == 1 then
+				gender = "Woman"
 			end
+			TriggerClientEvent('chat:addMessage', v,  {
+					template = '<div class="chat-message advert"><div class="chat-message-body"><strong>{0}:</strong><br><br> <strong>Civ ID:</strong> {1} <br><strong>First Name:</strong> {2} <br><strong>Last Name:</strong> {3} <br><strong>Birthdate:</strong> {4} <br><strong>Gender:</strong> {5} <br><strong>Nationality:</strong> {6}</div></div>',
+					args = {
+						"ID Card",
+						item.info.citizenid,
+						item.info.firstname,
+						item.info.lastname,
+						item.info.birthdate,
+						gender,
+						item.info.nationality
+					}
+				}
+			)
 		end
-	end)
+	end
 end)
 
 --#endregion Items
