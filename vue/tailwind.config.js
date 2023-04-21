e3f78be1a0ad9a49d8f263658439afb3de4efaa2
LordChunk <LordChunk@users.noreply.github.com>
2023-04-21 09:12:56 +0000

    Setup TailwindCSS, Sass & recommended extensions

---
+++ vue/tailwind.config.js
@@ -0,0 +1,7 @@
+/** @type {import('tailwindcss').Config} */
+export const content = ['./public/**/*.html', './src/**/*.{vue,js,ts,jsx,tsx}'];
+export const theme = {
+  extend: {},
+};
+export const plugins = [];
+
