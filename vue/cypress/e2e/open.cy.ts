eb63be1f643b05d13c5ef78b2128f169bb98a42b
Job <LordChunk@users.noreply.github.com>
2023-04-27 18:30:39 +0200

    Added drag and drop tests

--- vue/cypress/e2e/open.cy.ts
+++ vue/cypress/e2e/open.cy.ts
@@ -9,4 +9,39 @@ describe('Open inventory', () => {
   it('should open the inventory', () => {
     cy.get('.item-group').should('be.visible');
   })
+
+  it('should close the inventory', () => {
+    cy.get('.item-group').should('be.visible');
+    cy.intercept(
+      '/CloseInventory',
+      {
+        statusCode: 200,
+        body: {}
+      }
+    ).as('closeInventory');
+
+    cy.get('body').type('{esc}');
+
+    cy.get('.item-group').should('not.exist');
+    cy.wait('@closeInventory');
+  });
+
+  it.only('should swap items', () => {
+    const phone = cy.get('.item-container').contains('Phone').parent();
+    const idCard = cy.get('.item-container').contains('ID Card').parent();
+
+    phone.should('be.visible');
+    idCard.should('be.visible');
+
+    cy.intercept('/SetInventoryData', { statusCode: 200, body: {} })
+      .as('setInventoryData');
+
+    phone
+      .realMouseDown()
+      .realMouseMove(-150, 200, { position: 'center' })
+      .wait(300)
+      .realMouseUp();
+
+    cy.wait('@setInventoryData');
+  });
 })
--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <main class="w-screen h-screen text-white flex justify-center bg-gray-900 bg-opacity-30">
-    <!-- <pre v-text="inventory?.Items.value" /> -->
-    <ItemGroup :items="inventory.Items.value" @swap="onSwap" />
-
-    <ItemGroup v-if="container" :items="container.Items.value"/>
+    <template v-if="inventory.IsVisible.value">
+      <ItemGroup :items="inventory.Items.value" @swap="onSwap" />
+      <ItemGroup v-if="container" :items="container.Items.value"/>
+  </template>
   </main>
 </template>
 
 <script lang="ts" setup>
 import { inject } from 'vue';
 import { Inventory } from './Models/Inventory';
-import ItemGroup from './components/ItemGroup.vue';
 import { Container } from './Models/Container';
+import ItemGroup from './components/ItemGroup.vue';
 
 const inventory = inject<Inventory>('inventory')!;
 const container = inject<Container>('container');
