a457dc7a4012b9933c346618f539b9ff7a4bc270
Job <LordChunk@users.noreply.github.com>
2023-06-06 15:00:16 +0000

    Added box for displaying item metadata

---
+++ vue/src/components/ItemInfo.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1>Item info panel</h1>
+    <pre v-text="selectedItem?.info" />
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { Item } from "@/Models/Item/Item";
+import { inject, ref } from "vue";
+
+const selectedItem = inject(Item.SELECTED_ITEM, ref<Item | null>(null));
+</script>
