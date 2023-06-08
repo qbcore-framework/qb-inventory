550c3f383f04e3ffbf083ae0567f0434b3b7bcb1
LordChunk <LordChunk@users.noreply.github.com>
2023-06-08 15:02:31 +0000

    Fixed linting in cypress tests

--- vue/cypress/e2e/open.cy.ts
+++ vue/cypress/e2e/open.cy.ts
@@ -1,47 +1,45 @@
-import openEvent from '../fixtures/inventory-open-event.json';
+import openEvent from "../fixtures/inventory-open-event.json";
 
-describe('Open inventory', () => {
+describe("Open inventory", () => {
   beforeEach(() => {
-    cy.visit('http://localhost:3000')
-    cy.window().then(win => win.postMessage(openEvent));
-  })
-
-  it('should open the inventory', () => {
-    cy.get('.item-group').should('be.visible');
-  })
-
-  it('should close the inventory', () => {
-    cy.get('.item-group').should('be.visible');
-    cy.intercept(
-      '/CloseInventory',
-      {
-        statusCode: 200,
-        body: {}
-      }
-    ).as('closeInventory');
-
-    cy.get('body').type('{esc}');
-
-    cy.get('.item-group').should('not.exist');
-    cy.wait('@closeInventory');
+    cy.visit("http://localhost:3000");
+    cy.window().then((win) => win.postMessage(openEvent));
   });
 
-  it.only('should swap items', () => {
-    const phone = cy.get('.item-container').contains('Phone').parent();
-    const idCard = cy.get('.item-container').contains('ID Card').parent();
+  it("should open the inventory", () => {
+    cy.get(".item-group").should("be.visible");
+  });
 
-    phone.should('be.visible');
-    idCard.should('be.visible');
+  it("should close the inventory", () => {
+    cy.get(".item-group").should("be.visible");
+    cy.intercept("/CloseInventory", {
+      statusCode: 200,
+      body: {},
+    }).as("closeInventory");
 
-    cy.intercept('/SetInventoryData', { statusCode: 200, body: {} })
-      .as('setInventoryData');
+    cy.get("body").type("{esc}");
+
+    cy.get(".item-group").should("not.exist");
+    cy.wait("@closeInventory");
+  });
+
+  it.only("should swap items", () => {
+    const phone = cy.get(".item-container").contains("Phone").parent();
+    const idCard = cy.get(".item-container").contains("ID Card").parent();
+
+    phone.should("be.visible");
+    idCard.should("be.visible");
+
+    cy.intercept("/SetInventoryData", { statusCode: 200, body: {} }).as(
+      "setInventoryData"
+    );
 
     phone
       .realMouseDown()
-      .realMouseMove(-150, 200, { position: 'center' })
+      .realMouseMove(-150, 200, { position: "center" })
       .wait(300)
       .realMouseUp();
 
-    cy.wait('@setInventoryData');
+    cy.wait("@setInventoryData");
   });
-})
+});
--- vue/cypress/support/e2e.ts
+++ vue/cypress/support/e2e.ts
@@ -14,7 +14,7 @@
 // ***********************************************************
 
 // Import commands.js using ES2015 syntax:
-import './commands'
+import "./commands";
 import "cypress-real-events";
 
 // Alternatively you can use CommonJS syntax:
