3e350909ab8d76969d5f15bd62e923fad0d2ca29
multipl3x <allen@warpstudios.com.au>
2023-10-10 11:47:40 +0800

    Fix unique item swap

--- html/js/app.js
+++ html/js/app.js
@@ -1563,10 +1563,6 @@ function swap($fromSlot, $toSlot, $fromInv, $toInv, $toAmount) {
             );
         } else {
             if (fromData.amount == $toAmount) {
-                if (toData && toData.unique){
-                    InventoryError($fromInv, $fromSlot);
-                    return;
-                }
                 if (
                     toData != undefined &&
                     toData.combinable != null &&
