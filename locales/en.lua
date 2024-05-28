07a0d0147188d0147d1ba989db40244b5e4b9313
LeSiiN <103898231+LeSiiN@users.noreply.github.com>
2024-05-28 13:43:38 +0200

    added notify for ,,Cant Pickup the Bag''

--- locales/en.lua
+++ locales/en.lua
@@ -31,6 +31,7 @@ local Translations = {
         ['cgitem'] = 'Can\'t give item!',
         ['idne'] = 'Item Does Not Exist',
         ['pdne'] = 'Player Is Not Online',
+        ['c_pu_bag'] = 'Cannot carry the bag.',
     },
     inf_mapping = {
         ['opn_inv'] = 'Open Inventory',
