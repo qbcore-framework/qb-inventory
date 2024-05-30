5a5f2836629eb23ffb7b5610fe36df8384524fff
Kakarot <57848836+GhzGarage@users.noreply.github.com>
2024-05-30 08:29:28 -0500

    prevent unique stack, shop checks

--- html/app.js
+++ html/app.js
@@ -210,9 +210,9 @@ const InventoryContainer = Vue.createApp({
         getHotbarItemInSlot(slot) {
             return this.hotbarItems[slot - 1] || null;
         },
-        containerMouseDownAction(event){
+        containerMouseDownAction(event) {
             if (event.button === 0 && this.showContextMenu) {
-                this.showContextMenu = false
+                this.showContextMenu = false;
             }
         },
         handleMouseDown(event, slot, inventory) {
@@ -240,42 +240,52 @@ const InventoryContainer = Vue.createApp({
         moveItemBetweenInventories(item, sourceInventoryType) {
             const sourceInventory = sourceInventoryType === "player" ? this.playerInventory : this.otherInventory;
             const targetInventory = sourceInventoryType === "player" ? this.otherInventory : this.playerInventory;
-            const targetItemKey = Object.keys(targetInventory).find((key) => targetInventory[key] && targetInventory[key].name === item.name);
-            const targetItem = targetInventory[targetItemKey];
             const amountToTransfer = this.transferAmount !== null ? this.transferAmount : 1;
             let targetSlot = null;
 
             const sourceItem = sourceInventory[item.slot];
             if (!sourceItem || sourceItem.amount < amountToTransfer) {
-                console.error("Error: Insufficient amount of item in source inventory");
+                this.inventoryError(item.slot);
                 return;
             }
 
-            if (!targetItem) {
+            if (item.unique) {
+                targetSlot = this.findNextAvailableSlot(targetInventory);
+                if (targetSlot === null) {
+                    this.inventoryError(item.slot);
+                    return;
+                }
+
                 const newItem = {
                     ...item,
                     inventory: sourceInventoryType === "player" ? "other" : "player",
                     amount: amountToTransfer,
                 };
-
-                let added = false;
-                for (let slot = 1; slot <= this.totalSlots; slot++) {
-                    if (!targetInventory[slot]) {
-                        targetInventory[slot] = newItem;
-                        newItem.slot = slot;
-                        targetSlot = slot;
-                        added = true;
-                        break;
-                    }
-                }
-
-                if (!added) {
-                    console.error("Error: No free slots available in target inventory");
-                    return;
-                }
+                targetInventory[targetSlot] = newItem;
+                newItem.slot = targetSlot;
             } else {
-                targetItem.amount += amountToTransfer;
-                targetSlot = targetItem.slot;
+                const targetItemKey = Object.keys(targetInventory).find((key) => targetInventory[key] && targetInventory[key].name === item.name);
+                const targetItem = targetInventory[targetItemKey];
+
+                if (!targetItem) {
+                    const newItem = {
+                        ...item,
+                        inventory: sourceInventoryType === "player" ? "other" : "player",
+                        amount: amountToTransfer,
+                    };
+
+                    targetSlot = this.findNextAvailableSlot(targetInventory);
+                    if (targetSlot === null) {
+                        this.inventoryError(item.slot);
+                        return;
+                    }
+
+                    targetInventory[targetSlot] = newItem;
+                    newItem.slot = targetSlot;
+                } else {
+                    targetItem.amount += amountToTransfer;
+                    targetSlot = targetItem.slot;
+                }
             }
 
             sourceItem.amount -= amountToTransfer;
@@ -304,7 +314,7 @@ const InventoryContainer = Vue.createApp({
             this.dragStartX = event.clientX;
             this.dragStartY = event.clientY;
             this.dragStartInventoryType = inventoryType;
-            this.showContextMenu = false
+            this.showContextMenu = false;
         },
         createGhostElement(slotElement) {
             const ghostElement = slotElement.cloneNode(true);
@@ -410,6 +420,11 @@ const InventoryContainer = Vue.createApp({
         },
         handleItemDrop(targetInventoryType, targetSlot) {
             try {
+                const isShop = this.otherInventoryName.indexOf("shop-");
+                if (this.dragStartInventoryType === "other" && targetInventoryType === "other" && isShop !== -1) {
+                    return;
+                }
+
                 const targetSlotNumber = parseInt(targetSlot, 10);
                 if (isNaN(targetSlotNumber)) {
                     throw new Error("Invalid target slot number");
