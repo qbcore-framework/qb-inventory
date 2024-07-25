edb74e68918c9950e62a12dcb5263e6a31c2d7f3
Christian J Bennett <58433059+ChristianBDev@users.noreply.github.com>
2024-07-25 14:57:23 -0500

    Added Background-gradient

--- server/main.lua
+++ server/main.lua
@@ -174,7 +174,7 @@ RegisterNetEvent('qb-inventory:server:useItem', function(item)
             local dist = #(playerCoords - GetEntityCoords(targetPed))
             if dist < 3.0 then
                 TriggerClientEvent('chat:addMessage', v, {
-                    template = '<div class="chat-message advert" style="display: flex;"><div style="margin-right: 10px;"><i class="far fa-id-card" style="height: 100%;"></i><strong> {0}</strong><br> <strong>Civ ID:</strong> {1} <br><strong>First Name:</strong> {2} <br><strong>Last Name:</strong> {3} <br><strong>Birthdate:</strong> {4} <br><strong>Gender:</strong> {5} <br><strong>Nationality:</strong> {6}</div></div>',
+                    template = '<div class="chat-message advert" style="background: linear-gradient(to right, rgba(5, 5, 5, 0.6), #74807c); display: flex;"><div style="margin-right: 10px;"><i class="far fa-id-card" style="height: 100%;"></i><strong> {0}</strong><br> <strong>Civ ID:</strong> {1} <br><strong>First Name:</strong> {2} <br><strong>Last Name:</strong> {3} <br><strong>Birthdate:</strong> {4} <br><strong>Gender:</strong> {5} <br><strong>Nationality:</strong> {6}</div></div>',
                     args = {
                         'ID Card',
                         item.info.citizenid,
@@ -198,7 +198,7 @@ RegisterNetEvent('qb-inventory:server:useItem', function(item)
             local dist = #(playerCoords - GetEntityCoords(targetPed))
             if dist < 3.0 then
                 TriggerClientEvent('chat:addMessage', v, {
-                    template = '<div class="chat-message advert" style="display: flex;"><div style="margin-right: 10px;"><i class="far fa-id-card" style="height: 100%;"></i><strong> {0}</strong><br> <strong>First Name:</strong> {1} <br><strong>Last Name:</strong> {2} <br><strong>Birth Date:</strong> {3} <br><strong>Licenses:</strong> {4}</div></div>',
+                    template = '<div class="chat-message advert" style="background: linear-gradient(to right, rgba(5, 5, 5, 0.6), #657175); display: flex;"><div style="margin-right: 10px;"><i class="far fa-id-card" style="height: 100%;"></i><strong> {0}</strong><br> <strong>First Name:</strong> {1} <br><strong>Last Name:</strong> {2} <br><strong>Birth Date:</strong> {3} <br><strong>Licenses:</strong> {4}</div></div>',
                     args = {
                         'Drivers License',
                         item.info.firstname,
