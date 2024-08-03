c7ec1774170822702a6191eb1cccc4fc52a83fd6
Ed3n18 <65429070+Ed3n18@users.noreply.github.com>
2024-08-03 18:59:50 +0200

    Update es.lua
    Fixed GetConvar

--- locales/es.lua
+++ locales/es.lua
@@ -48,7 +48,10 @@ local Translations = {
     },
 }
 
-Lang = Lang or Locale:new({
-    phrases = Translations,
-    warnOnMissing = true
-})
+if GetConvar('qb_locale', 'en') == 'es' then
+    Lang = Locale:new({
+        phrases = Translations,
+        warnOnMissing = true,
+        fallbackLang = Lang,
+    })
+end
