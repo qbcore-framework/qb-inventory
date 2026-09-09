989b27a1195348923ff834130ab98f82c63164d6
Joel <34402846+Qwerty1Verified@users.noreply.github.com>
2026-09-08 21:45:42 -0700

    chore: bump version to 2.2.2

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,7 +3,7 @@ game 'gta5'
 lua54 'yes'
 author 'Kakarot'
 description 'Player inventory system providing a variety of features for storing and managing items'
-version '2.2.1'
+version '2.2.2'
 
 shared_scripts {
     '@qb-core/shared/locale.lua',
